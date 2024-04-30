3bb5e4fe21cd601d2b9c0c75dc17975aee04f1e4
Joseefrdz <jf.developr@gmail.com>
2024-04-30 13:38:15 +0200

    corrección archivoImg y archivoMusic

--- src/app/archivoImg.ts
+++ src/app/archivoImg.ts
@@ -5,31 +5,31 @@ export interface imagen{
 }
 
 export const paisajes:Array<imagen>=[
-    {tipo:"paisaje",nombre:"cielo",url:'/assets/imgs/Paisaje/descarga.gif'},
-    {tipo:"paisaje",nombre:"campo",url:'/assets/imgs/Paisaje/fondo.gif'},
-    {tipo:"paisaje",nombre:"acantilado",url:'/assets/imgs/Paisaje/paisaje acantilado.png'},
-    {tipo:"paisaje",nombre:"city1",url:'/assets/imgs/Paisaje/_9d90a91e-5b5d-4dab-9ef1-fc1cfa70065d.jpeg'},
-    {tipo:"paisaje",nombre:"city2",url:'/assets/imgs/Paisaje/_68181515-bedb-40e0-b1b0-20489190bcce.jpeg'},
-    {tipo:"paisaje",nombre:"prado",url:'/assets/imgs/Paisaje/desktop-wallpaper-white-clouds-illustration-pixel-art-8-bit-•-for-you-cloud-pixel-art.jpg'},
+    {tipo:"paisaje",nombre:"cielo",url:'assets/imgs/Paisaje/descarga.gif'},
+    {tipo:"paisaje",nombre:"campo",url:'assets/imgs/Paisaje/fondo.gif'},
+    {tipo:"paisaje",nombre:"acantilado",url:'assets/imgs/Paisaje/paisaje acantilado.png'},
+    {tipo:"paisaje",nombre:"city1",url:'assets/imgs/Paisaje/_9d90a91e-5b5d-4dab-9ef1-fc1cfa70065d.jpeg'},
+    {tipo:"paisaje",nombre:"city2",url:'assets/imgs/Paisaje/_68181515-bedb-40e0-b1b0-20489190bcce.jpeg'},
+    {tipo:"paisaje",nombre:"prado",url:'assets/imgs/Paisaje/desktop-wallpaper-white-clouds-illustration-pixel-art-8-bit-•-for-you-cloud-pixel-art.jpg'},
 ];
 export const fondos:Array<imagen>=[
-    {tipo:"fondo",nombre:"banco",url:'/assets/imgs/Fondo/Fondo_Banco.png'},
-    {tipo:"fondo",nombre:"acantilado",url:'/assets/imgs/Fondo/acantilado.png'},
+    {tipo:"fondo",nombre:"banco",url:'assets/imgs/Fondo/Fondo_Banco.png'},
+    {tipo:"fondo",nombre:"acantilado",url:'assets/imgs/Fondo/acantilado.png'},
 ];
 export const personajes:Array<imagen>=[
-    {tipo:"person",nombre:"Jake",url:'/assets/imgs/Person/2a9p.gif'},
-    {tipo:"person",nombre:"Finn",url:'/assets/imgs/Person/Finn_original_sentado-1-_1.png'},
-    {tipo:"person",nombre:"Rick",url:'/assets/imgs/Person/WG8Q.gif'},
-    {tipo:"person",nombre:"Bulbasaur",url:'/assets/imgs/Person/WnES.gif'},
-    {tipo:"person",nombre:"Rick2",url:'/assets/imgs/Person/WS2c.gif'},
-    {tipo:"person",nombre:"Squirtel",url:'/assets/imgs/Person/ZDcp.gif'},    
+    {tipo:"person",nombre:"Jake",url:'assets/imgs/Person/2a9p.gif'},
+    {tipo:"person",nombre:"Finn",url:'assets/imgs/Person/Finn_original_sentado-1-_1.png'},
+    {tipo:"person",nombre:"Rick",url:'assets/imgs/Person/WG8Q.gif'},
+    {tipo:"person",nombre:"Bulbasaur",url:'assets/imgs/Person/WnES.gif'},
+    {tipo:"person",nombre:"Rick2",url:'assets/imgs/Person/WS2c.gif'},
+    {tipo:"person",nombre:"Squirtel",url:'assets/imgs/Person/ZDcp.gif'},    
 
 ];
 export const extras:Array<imagen>=[
-    {tipo:"extra",nombre:"Kirby",url:'/assets/imgs/Person/45RT.gif'},
-    {tipo:"extra",nombre:"BM0",url:'/assets/imgs/Person/Paz.gif'},
-    {tipo:"extra",nombre:"Yigglipuf",url:'/assets/imgs/Person/Ve5.gif'},
-    {tipo:"extra",nombre:"BM0",url:'/assets/imgs/Person/BM0.png'},
-    {tipo:"extra",nombre:"pinguino",url:'/assets/imgs/Person/Z5cP.gif'},
-    {tipo:"extra",nombre:"gato",url:'/assets/imgs/Person/bdP.gif'},
+    {tipo:"extra",nombre:"Kirby",url:'assets/imgs/Person/45RT.gif'},
+    {tipo:"extra",nombre:"BM0",url:'assets/imgs/Person/Paz.gif'},
+    {tipo:"extra",nombre:"Yigglipuf",url:'assets/imgs/Person/Ve5.gif'},
+    {tipo:"extra",nombre:"BM0",url:'assets/imgs/Person/BM0.png'},
+    {tipo:"extra",nombre:"pinguino",url:'assets/imgs/Person/Z5cP.gif'},
+    {tipo:"extra",nombre:"gato",url:'assets/imgs/Person/bdP.gif'},
 ];
